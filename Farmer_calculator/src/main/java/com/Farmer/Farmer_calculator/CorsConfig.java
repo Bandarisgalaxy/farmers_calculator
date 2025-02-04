905699b9ceb4ebd29fc464ae25eb4f2c363a54d4
Bandarisgalaxy <harshithbandari810@gmail.com>
2025-02-04 15:53:17 +0530

    Add files via upload

---
+++ Farmer_calculator/src/main/java/com/Farmer/Farmer_calculator/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.Farmer.Farmer_calculator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")  // Specify your frontend URL here
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Specify allowed methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);  // Allow credentials (cookies, authorization headers, etc.)
+    }
+}
+
